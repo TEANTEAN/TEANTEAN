02185a946778fe393f69d68b8a9e34b729ea376a
Chuanyuan <liuchuanyuan@gmail.com>
2021-05-17 02:32:33 +1000

    Created home page basic layout

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { NextPage } from "next";
+import {NextPage} from "next";
 import Head from "next/head";
-import { GeneralButton } from "components/Buttons";
+import {GeneralButton} from "components/Buttons";
 
-const Home: NextPage = () => (
-  <div>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <h1>genyus Rountable</h1>
-    <GeneralButton href="/buttons">Button Library</GeneralButton>
-    <GeneralButton href="/form">Form example</GeneralButton>
-  </div>
+const Index: NextPage = () => (
+    <div>
+        <Head>
+            <title>Create Next App</title>
+            <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <GeneralButton href='/home'> Home page </GeneralButton>
+        <GeneralButton href='/buttons'>Button Library</GeneralButton>
+        <GeneralButton href='/form'>Form example</GeneralButton>
+    </div>
 );
 
-export default Home;
+export default Index;
