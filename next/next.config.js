f964688a746c8877ab2f518ed99a178c8b9a3729
Sam Webster <swebster1@student.unimelb.edu.au>
2021-08-16 18:05:05 +1000

    update env variables to point at dev environment

--- next/next.config.js
+++ next/next.config.js
@@ -11,8 +11,8 @@ module.exports = (phase) => {
           NEXT_PUBLIC_ENV_NAME: process.env.NEXT_PUBLIC_ENV_NAME,
           NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
           BUILD_HOST: process.env.NEXT_PUBLIC_HOST,
+          NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-          BACKEND_URL: process.env.BACKEND_URL,
         },
       };
 
@@ -24,8 +24,8 @@ module.exports = (phase) => {
           NEXT_PUBLIC_ENV_NAME: process.env.NEXT_PUBLIC_ENV_NAME,
           NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
           BUILD_HOST: process.env.NEXT_PUBLIC_HOST,
+          NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-          BACKEND_URL: process.env.BACKEND_URL,
         },
       };
   }
