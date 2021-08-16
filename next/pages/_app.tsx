66dbef4de7d96a1df07717717f175137e61e3869
Sam Webster <swebster1@student.unimelb.edu.au>
2021-08-16 18:06:20 +1000

    add react-query dependency

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -8,23 +8,30 @@ import theme from "styles/theme";
 import DateFnsUtils from "@date-io/date-fns";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { Provider } from "next-auth/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Provider session={pageProps.session}>
-    <Head>
-      <meta name="theme-color" content={theme.palette.primary.main} />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1"
-      />
-    </Head>
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </MuiPickersUtilsProvider>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider session={pageProps.session}>
+      <Head>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1"
+        />
+      </Head>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </MuiPickersUtilsProvider>
+    </Provider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 export default App;
