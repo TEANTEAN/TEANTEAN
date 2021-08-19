3583fed878f179f5b8fc85e0e6a371d41d2c2267
Pei-Chen Chen <p.chen24@student.unimelb.edu.au>
2021-08-19 20:23:55 +1000

    added header and footer as an example into the index page.

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { GeneralButton } from "components/Buttons";
+import Header from "components/Header";
+import Footer from "components/Footer";
 import Image from "next/image";
 
 const Home: NextPage = () => (
   <div>
+    <Header/>
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
@@ -14,6 +17,7 @@ const Home: NextPage = () => (
     <GeneralButton href="/buttons">Button Library</GeneralButton>
     <GeneralButton href="/form">Form example</GeneralButton>
     <GeneralButton href="/login">Login</GeneralButton>
+    <Footer/>
   </div>
 );
 
