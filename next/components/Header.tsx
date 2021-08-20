b8fd4b60b7e8b26597d41caf78f84a4b7a6ad602
Chuanyuan <liuchuanyuan@gmail.com>
2021-08-20 18:45:54 +1000

    Removed unnecessary styles

--- next/components/Header.tsx
+++ next/components/Header.tsx
@@ -1,28 +1,5 @@
 import React,{ CSSProperties } from "react";
-import {Button, Grid} from "@material-ui/core";
-
-//ERIC
-//Return the login button for header
-function LoginB() : JSX.Element {
-    const style: CSSProperties = {
-        padding: `0 ${14}px`,
-        borderRadius: "50px",
-        textTransform: "none",
-        fontFamily: "Work Sans, sans-serif",
-        letterSpacing: "1.24px",
-        lineHeight: "18px",
-        fontWeight: 500,
-        fontSize: 14,
-        height: `${Math.max(Math.floor(14*2.5), 30)}px`, // a work around because no max-content option 
-    };
-  return (
-    <Button style={style} 
-                        variant="contained" color="primary" 
-                        size="small" disabled={false}> 
-                {"LOGIN"}
-    </Button>
-  );
-}
+import {Button, Grid, Divider} from "@material-ui/core";
 
 function Header() : JSX.Element{
     const LogoStyleTop: CSSProperties = {
@@ -47,19 +24,22 @@ function Header() : JSX.Element{
         fontSize: 40,
     };
     return(
-        <Grid style={{margin: 0}}>
-            <Grid item xs={12} style={{marginTop: "10px", marginLeft: "10px"}}>
-            <a style={LogoStyleTop}>gen</a>
-            <a style={LogoStyleTop2}>y</a>
-            <a style={LogoStyleTop}>us</a><br/>
-            <div style={{marginTop: "3px"}}>
+        <Grid container alignItems='center' justifyContent='flex-end'>
+            <Grid item xs={9}>
+                <a style={LogoStyleTop}>gen</a>
+                <a style={LogoStyleTop2}>y</a>
+                <a style={LogoStyleTop}>us</a>
+                <br/>
                 <a style={LogoStyleBot}>Roundtable</a>
-            </div>
             </Grid>
-            <Grid item xs={12} style={{marginTop: "-60px", float: "right", marginRight: "25px"}}>
-                <LoginB />
+            <Grid item xs={3} style={{textAlign: 'right'}}>
+                <Button variant='contained' color="primary">
+                    Login
+                </Button>
             </Grid>
-            <hr style={{borderTopColor:"#1989B5", marginTop: "15px", marginLeft:"10px", marginRight:"15px"}} />
+            <Grid item xs={12}>
+                <Divider style={{height: "4px"}}/>
+            </Grid>
         </Grid>
     );
 }
