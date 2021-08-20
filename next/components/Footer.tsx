843751842f53c450c6604f3f7b8ccf18b71c535b
Chuanyuan <liuchuanyuan@gmail.com>
2021-08-20 18:45:13 +1000

    simplified

--- next/components/Footer.tsx
+++ next/components/Footer.tsx
@@ -2,14 +2,12 @@ import React from "react";
 
 function Footer() : JSX.Element {
     return (
-    <section style={{ position: "absolute", bottom: "0", right:"0",left:"0",backgroundColor:"#1989B5"}}>
-        <div style={{float: "right", marginRight:"20px"}}>
-            <img src= {"/telegram.png"} height="30" style={{marginTop: "10px", marginBottom: "5px", marginLeft: "10px"}}/>
-            <img src= {"/facebook.png"} height="30" style={{marginTop: "10px", marginBottom: "5px", marginLeft: "10px"}}/>
-            <img src= {"/youtube.png"} height="30" style={{marginTop: "10px", marginBottom: "5px", marginLeft: "10px"}}/>
-            <img src= {"/twitter.png"} height="30" style={{marginTop: "10px", marginBottom: "5px", marginLeft: "10px"}}/>
+        <div style={{backgroundColor:"#1989B5", width: "100%", textAlign: "right", paddingRight: "20px"}}>
+            <img src= {"/telegram.png"} height="30" />
+            <img src= {"/facebook.png"} height="30" />
+            <img src= {"/youtube.png"} height="30" />
+            <img src= {"/twitter.png"} height="30" />
         </div>
-    </section>
     );
 }
 
