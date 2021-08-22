34ab99c83f5addeae7f6089495b523bb6b22441c
yang zhou <yang.zhou.q15@gmail.com>
2021-08-22 15:14:38 +1000

    Create account & series TS fix

--- next/pages/createaccount.tsx
+++ next/pages/createaccount.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from "react";
+// import { makeStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { useForm } from "react-hook-form";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 import { NextPage } from "next";
 import Form, {
   TextField,
   AutocompleteField,
   DatePickerField,
 } from "components/Form";
-
 
 interface FormValues {
   username: String;
@@ -25,7 +26,7 @@ interface FormValues {
   level: Level;
   org: Org;
   date: Date;
-  //input: File;
+  // input: File;
 }
 
 interface Level {
@@ -39,27 +40,15 @@ interface Org {
 }
 
 const levels: Level[] = [
-  { id: 1,
-    level: "Peer Leader",
-  },
-  { id: 2,
-    level: "genyus Admin",
-  },
-  { id: 3,
-    level: "Research Partner",
-  },
+  { id: 1, level: "Peer Leader" },
+  { id: 2, level: "genyus Admin" },
+  { id: 3, level: "Research Partner" },
 ];
 
 const orgs: Org[] = [
-  { id: 1,
-    org: "The University of Melbourne",
-  },
-  { id: 2,
-    org: "Young Stroke Foundation",
-  },
-  { id: 3,
-    org: "Monash University",
-  },
+  { id: 1, org: "The University of Melbourne" },
+  { id: 2, org: "Young Stroke Foundation" },
+  { id: 3, org: "Monash University" },
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -68,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    margin: 'auto',
+    margin: "auto",
     maxWidth: 500,
   },
   image: {
@@ -76,164 +65,174 @@ const useStyles = makeStyles((theme) => ({
     height: 128,
   },
   img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
   },
 }));
 
 const CreateAccount: NextPage = () => {
   const classes = useStyles();
-  const [submittedValues, setSubmittedValues] = React.useState<FormValues>(
-    null
-  );
-
+  const [submittedValues, setSubmittedValues] =
+    React.useState<FormValues>(null);
+  const methods = useForm<FormValues>();
   const onSubmit = (data: FormValues) => {
     setSubmittedValues(data);
   };
 
   return (
-    <> 
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-      <Typography gutterBottom variant="subtitle1">
-                  Account Management
-                </Typography>
+    <>
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Typography gutterBottom variant="subtitle1">
+            Account Management
+          </Typography>
+          <Form<FormValues> methods={methods} onSubmit={onSubmit}>
+            <TextField
+              control={methods.control}
+              name="username"
+              label="Username"
+              rules={{
+                minLength: {
+                  value: 5,
+                  message: "Username must be 5 chars long",
+                },
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-      <Form<FormValues> onSubmit={onSubmit}>
-               
-          <TextField       
-          name="username"
-          label="Username"
-          rules={{
-            minLength: {
-              value: 5,
-              message: "Username must be 5 chars long",
-            },
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <TextField
+              control={methods.control}
+              name="password"
+              label="Password"
+              rules={{
+                minLength: {
+                  value: 5,
+                  message: "Password must be 5 chars long",
+                },
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-<TextField
-          name="password"
-          label="Password"
-          rules={{
-            minLength: {
-              value: 5,
-              message: "Password must be 5 chars long",
-            },
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <TextField
+              control={methods.control}
+              name="email"
+              label="Email"
+              rules={{
+                minLength: {
+                  value: 5,
+                  message: "Email must be 5 chars long",
+                },
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-<TextField
-          name="email"
-          label="Email"
-          rules={{
-            minLength: {
-              value: 5,
-              message: "Email must be 5 chars long",
-            },
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <TextField
+              control={methods.control}
+              name="firstname"
+              label="First Name"
+              rules={{
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-       <TextField
-          name="firstname"
-          label="First Name"
-          rules={{   
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <TextField
+              control={methods.control}
+              name="lastname"
+              label="Last Name"
+              rules={{
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-<TextField
-          name="lastname"
-          label="Last Name"
-          rules={{
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }} />
+            <TextField
+              control={methods.control}
+              name="phone"
+              label="Phone Number"
+              rules={{}}
+            />
 
-       <TextField
-          name="phone"
-          label="Phone Number"
-          rules={{
-          }}
-        />
+            <AutocompleteField<Level>
+              control={methods.control}
+              name="level"
+              label="genyus User"
+              options={levels}
+              getOptionLabel={(level) => level.level}
+            />
 
-    <AutocompleteField<Level>
-          name="level"
-          label="genyus User"
-          options={levels}
-          getOptionLabel={(level) => level.level}
-          getOptionValue={(level) => level.id}
-        />
+            <AutocompleteField<Org>
+              control={methods.control}
+              name="org"
+              label="Organisation"
+              options={orgs}
+              getOptionLabel={(org) => org.org}
+            />
 
-        <AutocompleteField<Org>
-          name="org"
-          label="Organisation"
-          options={orgs}
-          getOptionLabel={(org) => org.org}
-          getOptionValue={(org) => org.id}
-        />
+            <DatePickerField
+              control={methods.control}
+              name="date"
+              label="Date Register"
+              defaultValue={new Date()}
+            />
 
-       <DatePickerField 
-          name="date" 
-          label="Date Register" 
-          defaultValue={new Date()} />
+            <Typography gutterBottom variant="subtitle2">
+              Profile Picture Upload
+            </Typography>
 
-<Typography gutterBottom variant="subtitle2">
-                  Profile Picture Upload
-                </Typography>
+            <div className={classes.root}>
+              <input
+                accept="image/*"
+                // className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+              />
+              <label htmlFor="contained-button-file">
+                <Button variant="contained" color="primary" component="span">
+                  Upload
+                </Button>
+              </label>
+              <input
+                accept="image/*"
+                // className={classes.input}
+                id="icon-button-file"
+                type="file"
+              />
+              <label htmlFor="icon-button-file">
+                <IconButton
+                  color="primary"
+                  aria-label="upload picture"
+                  component="span"
+                >
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+            </div>
+          </Form>
+        </Paper>
+      </div>
 
-<div className={classes.root}>
-      <input
-        accept="image/*"
-        //className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
-      <input accept="image/*" 
-      //className={classes.input} 
-      id="icon-button-file" 
-      type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-    </div>
-
-      </Form>
-      </Paper>
-    </div>
-
-    {submittedValues && (
+      {submittedValues && (
         <Typography variant="body1">
           Submitted: {JSON.stringify(submittedValues)}
         </Typography>
-      )}   
+      )}
     </>
   );
 };
--- next/pages/createseries.tsx
+++ next/pages/createseries.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from "react";
+// import { makeStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { useForm } from "react-hook-form";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 import { NextPage } from "next";
 import Form, {
   TextField,
   AutocompleteField,
   DatePickerField,
 } from "components/Form";
-
 
 interface FormValues {
   title: String;
@@ -35,25 +36,19 @@ interface Org {
 }
 
 const orgs: Org[] = [
-  { id: 1,
-    org: "The University of Melbourne",
-  },
-  { id: 2,
-    org: "Young Stroke Foundation",
-  },
-  { id: 3,
-    org: "Monash University",
-  },
+  { id: 1, org: "The University of Melbourne" },
+  { id: 2, org: "Young Stroke Foundation" },
+  { id: 3, org: "Monash University" },
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  
+
   paper: {
     padding: theme.spacing(2),
-    margin: 'auto',
+    margin: "auto",
     maxWidth: 500,
   },
   image: {
@@ -61,117 +56,122 @@ const useStyles = makeStyles((theme) => ({
     height: 128,
   },
   img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
   },
 }));
 
 const CreateSeries: NextPage = () => {
   const classes = useStyles();
-  const [submittedValues, setSubmittedValues] = React.useState<FormValues>(
-    null
-  );
-
+  const [submittedValues, setSubmittedValues] =
+    React.useState<FormValues>(null);
+  const methods = useForm<FormValues>();
   const onSubmit = (data: FormValues) => {
     setSubmittedValues(data);
   };
 
   return (
-    <> 
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-      <Typography gutterBottom variant="subtitle1">
-                  Series Management
-                </Typography>
+    <>
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Typography gutterBottom variant="subtitle1">
+            Series Management
+          </Typography>
 
-      <Form<FormValues> onSubmit={onSubmit}>
+          <Form<FormValues> methods={methods} onSubmit={onSubmit}>
+            <Typography gutterBottom variant="subtitle2">
+              Series Information
+            </Typography>
 
-             <Typography gutterBottom variant="subtitle2">
-                  Series Information
-                </Typography>
+            <TextField
+              control={methods.control}
+              name="title"
+              label="Series Name"
+              rules={{
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-          <TextField       
-          name="title"
-          label="Series Name"
-          rules={{
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <TextField
+              control={methods.control}
+              name="title"
+              label="Series Description"
+              //  multiline
+              //   rows={4}
+              rules={{
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              }}
+            />
 
-        <TextField       
-          name="title"
-          label="Series Description"
-          
-        //  multiline
-       //   rows={4}
-          rules={{
-            required: {
-              value: true,
-              message: "This field is required",
-            },
-          }}
-        />
+            <AutocompleteField<Org>
+              control={methods.control}
+              name="org"
+              label="Organisation"
+              options={orgs}
+              getOptionLabel={(org) => org.org}
+            />
 
+            <Typography gutterBottom variant="subtitle2">
+              Preview Vedio & Photo Upload
+            </Typography>
 
-        <AutocompleteField<Org>
-          name="org"
-          label="Organisation"
-          options={orgs}
-          getOptionLabel={(org) => org.org}
-          getOptionValue={(org) => org.id}
-        />
+            <div className={classes.root}>
+              <input
+                accept="image/*"
+                // className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+              />
+              <label htmlFor="contained-button-file">
+                <Button variant="contained" color="primary" component="span">
+                  Upload
+                </Button>
+              </label>
+              <input
+                accept="image/*"
+                // className={classes.input}
+                id="icon-button-file"
+                type="file"
+              />
+              <label htmlFor="icon-button-file">
+                <IconButton
+                  color="primary"
+                  aria-label="upload picture"
+                  component="span"
+                >
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+            </div>
 
-<Typography gutterBottom variant="subtitle2">
-                  Preview Vedio & Photo Upload
-                </Typography>
+            <Typography gutterBottom variant="subtitle2">
+              Roundtable Management (Calendly)
+            </Typography>
 
-                <div className={classes.root}>
-      <input
-        accept="image/*"
-        //className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
-      <input accept="image/*" 
-      //className={classes.input} 
-      id="icon-button-file" 
-      type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-    </div>
+            <DatePickerField
+              control={methods.control}
+              name="date"
+              label="Date Roundtable"
+              defaultValue={new Date()}
+            />
+          </Form>
+        </Paper>
+      </div>
 
-    <Typography gutterBottom variant="subtitle2">
-                  Roundtable Management (Calendly)
-                </Typography>
-
-       <DatePickerField 
-          name="date" 
-          label="Date Roundtable" 
-          defaultValue={new Date()} />
-
-      </Form>
-      </Paper>
-    </div>
-
-    {submittedValues && (
+      {submittedValues && (
         <Typography variant="body1">
           Submitted: {JSON.stringify(submittedValues)}
         </Typography>
-      )}   
+      )}
     </>
   );
 };
