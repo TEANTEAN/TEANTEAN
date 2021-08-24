97beb1095ee520bf57be697cd19a3fdc53df9bce
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-08-24 13:44:37 +0800

    v1.0 can retrieve files, displayed by logs()

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -4,6 +4,35 @@ import Head from "next/head";
 import { GeneralButton } from "components/Buttons";
 import Image from "next/image";
 
+import { google, drive_v3 } from "googleapis";
+
+// later change the following vars to env files
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'server-credentials.json',
+    scopes: ['https://www.googleapis.com/auth/drive.metadata.readonly']
+})
+
+const drive: drive_v3.Drive = google.drive({
+    version: 'v3',
+    auth,
+});
+
+drive.files.list({
+  pageSize: 10,
+  fields: 'nextPageToken, files(id, name)',
+}, (err, res) => {
+  if (err) return console.log('The API returned an error: ' + err);
+  const files = res.data.files;
+  if (files.length) {
+    console.log('Files:');
+    files.map((file) => {
+      console.log(`${file.name} (${file.id})`);
+    });
+  } else {
+    console.log('No files found.');
+  }
+});
+
 const Home: NextPage = () => (
   <div>
     <Head>
@@ -18,6 +47,7 @@ const Home: NextPage = () => (
     <GeneralButton href="/createaccount">Create Account</GeneralButton>
     <GeneralButton href="/createseries">Create Series</GeneralButton>
     <GeneralButton href="/landing">Landing</GeneralButton>
+    <GeneralButton href="/upload">Upload Example</GeneralButton>
   </div>
 );
 
