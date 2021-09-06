617f9531963c71aeb831d850d1bc684c8ca60eff
Sam Webster <swebster1@student.unimelb.edu.au>
2021-09-06 23:05:27 +1000

    add new env variables

--- next/next.config.js
+++ next/next.config.js
@@ -13,6 +13,17 @@ module.exports = (phase) => {
           BUILD_HOST: process.env.NEXT_PUBLIC_HOST,
           NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+          GDRIVE_TYPE: process.env.GDRIVE_TYPE,
+          GDRIVE_PROJECT_ID: process.env.GDRIVE_PROJECT_ID,
+          GDRIVE_PRIVATE_KEY_ID: process.env.GDRIVE_PRIVATE_KEY_ID,
+          GDRIVE_PRIVATE_KEY: process.env.GDRIVE_PRIVATE_KEY,
+          GDRIVE_CLIENT_EMAIL: process.env.GDRIVE_CLIENT_EMAIL,
+          GDRIVE_CLIENT_ID: process.env.GDRIVE_CLIENT_ID,
+          GDRIVE_AUTH_URI: process.env.GDRIVE_AUTH_URI,
+          GDRIVE_TOKEN_URI: process.env.GDRIVE_TOKEN_URI,
+          GDRIVE_AUTH_PROVIDER_X509_CERT_URL:
+            process.env.GDRIVE_AUTH_PROVIDER_X509_CERT_URL,
+          GDRIVE_CLIENT_X509_CERT_URL: process.env.GDRIVE_CLIENT_X509_CERT_URL,
         },
       };
 
@@ -26,8 +37,18 @@ module.exports = (phase) => {
           BUILD_HOST: process.env.NEXT_PUBLIC_HOST,
           NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        },    
+          GDRIVE_TYPE: process.env.GDRIVE_TYPE,
+          GDRIVE_PROJECT_ID: process.env.GDRIVE_PROJECT_ID,
+          GDRIVE_PRIVATE_KEY_ID: process.env.GDRIVE_PRIVATE_KEY_ID,
+          GDRIVE_PRIVATE_KEY: process.env.GDRIVE_PRIVATE_KEY,
+          GDRIVE_CLIENT_EMAIL: process.env.GDRIVE_CLIENT_EMAIL,
+          GDRIVE_CLIENT_ID: process.env.GDRIVE_CLIENT_ID,
+          GDRIVE_AUTH_URI: process.env.GDRIVE_AUTH_URI,
+          GDRIVE_TOKEN_URI: process.env.GDRIVE_TOKEN_URI,
+          GDRIVE_AUTH_PROVIDER_X509_CERT_URL:
+            process.env.GDRIVE_AUTH_PROVIDER_X509_CERT_URL,
+          GDRIVE_CLIENT_X509_CERT_URL: process.env.GDRIVE_CLIENT_X509_CERT_URL,
+        },
       };
   }
-
 };
