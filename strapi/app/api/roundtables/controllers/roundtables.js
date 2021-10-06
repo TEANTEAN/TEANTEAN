3b7a303265ac259e008d201e1675d7d617ef13f7
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-10-06 10:46:59 +0800

    v1

--- strapi/app/api/roundtables/controllers/roundtables.js
+++ strapi/app/api/roundtables/controllers/roundtables.js
@@ -1,8 +1,92 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const axios = require("axios");
 
-module.exports = {};
+/***
+ * Currently no series identifer is given. So have to return all rountables regardless which series they belong to.
+ * If the API calls can container series identifier, this api can be modified to only
+ * return the corresponding roundtables, which I think is more reasonable.
+ */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+  async find(ctx) {
+    // const entity = await strapi.query("roundtables").find({'series': });
+
+    /***
+     * Get roundtable collections from Strapi
+     */
+    const entity = await strapi.services["roundtables"].find();
+    const roundtables = sanitizeEntity(entity, {
+      model: strapi.models["roundtables"],
+    });
+
+    /***
+     * Get All bookings from Calendly
+     */
+    const meURLresponse = await axios({
+      method: "get",
+      url: "https://api.calendly.com/users/me",
+      headers: {
+        Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+      },
+    });
+
+    const user = meURLresponse.resource.uri;
+    const queryURL =
+      "https://api.calendly.com/scheduled_events?status=active&" +
+      meURLresponse.resource.uri +
+      "&sort=start_time:asc";
+
+    const bookingResponse = await axios({
+      method: "get",
+      url: queryURL,
+      headers: {
+        Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+      },
+    });
+
+    var bookings = bookingResponse.collection;
+
+    // continue to retrieve bookings if there is a next_page
+    var nextPageLink = bookingResponse.pagination.next_page;
+    while (nextPageLink !== "null") {
+      var nextResponse = await axios({
+        method: "get",
+        url: nextPageLink,
+        headers: {
+          Authorization: `Bearer ${process.env.CALENDLY_TOKEN}`,
+        },
+      });
+      bookings.concat(nextResponse.collection);
+      nextPageLink = nextResponse.pagination.next_page;
+    }
+
+    /***
+     * Merge results from Strapi and Calendly together
+     */
+    var finalRes = [];
+    for (const roundtable of roundtables) {
+      const resource = response.data.resource;
+      const data = {
+        id: series.id,
+        title: series.title,
+        description: resource.description_plain,
+        videoLink: series.videoLink,
+        researchPartner: series.researchPartner.username,
+        schedulingUrl: resource.scheduling_url,
+      };
+      finalRes.push(data);
+    }
+
+    return finalRes;
+  },
+};
